6710c4e45b0acd37c7f50b77b85a6803ddb4afd4
Pietro Bondioli <pietrobondiolideveloper@gmail.com>
2021-04-15 10:11:40 -0300

    feat: 'skillItem' component now uses 'next/Image' instead of 'img' tag

--- src/containers/About/Skills/SkillItem/index.jsx
+++ src/containers/About/Skills/SkillItem/index.jsx
@@ -1,3 +1,6 @@
+// React/Next Components
+import Image from 'next/image';
+
 // Styles
 import styles from '@styles/pages/About/SkillItem.module.scss';
 
@@ -6,7 +9,7 @@ const SkillsItem = (props) => {
   return (
     <a href={skillLink} className={styles.skill} target="blank">
       <div className={styles.name}>{skillName}</div>
-      <img className={styles.image} src={skillImage} alt={skillImageAlt} />
+      <Image className={styles.image} src={skillImage} alt={skillImageAlt} width={32} height={32} />
     </a>
   );
 };
