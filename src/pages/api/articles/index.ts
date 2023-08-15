f8ea00695b35420fe2a78caac0b8bff2785418c3
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-08-15 19:38:36 -0300

    feat: add filters to get articles route, also content is not returned on get all

--- src/pages/api/articles/index.ts
+++ src/pages/api/articles/index.ts
@@ -4,7 +4,32 @@ import { prisma } from "@/server/db"
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === `GET`) {
-        const articles = await prisma.article.findMany()
+        const { lang, slug } = req.query
+
+        const whereClause: any = {}
+        if (lang) whereClause.lang = lang
+        if (slug) whereClause.slug = slug
+
+        const articles = await prisma.article.findMany({
+            where: whereClause,
+            select: {
+                id: true,
+                slug: true,
+                title: true,
+                lang: true,
+                published_at: true,
+                last_modified: true,
+                author_name: true,
+                excerpt: true,
+                image_url: true,
+                image_alt: true,
+                comments: true,
+                category: true,
+                // Explicitly skip content
+                content: false,
+            },
+        })
+
         return res.json(articles)
     }
 
