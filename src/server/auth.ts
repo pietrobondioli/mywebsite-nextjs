81bd54797a3b41289629475e637f77ecad9ec94b
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-08-15 12:01:53 -0300

    feat: get session now throws an error on invalid session

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -46,15 +46,6 @@ export const authOptions: NextAuthOptions = {
             clientId: env.GITHUB_ID,
             clientSecret: env.GITHUB_SECRET,
         }),
-        /**
-         * ...add more providers here.
-         *
-         * Most other providers require a bit more work than the Discord provider. For example, the
-         * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-         * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-         *
-         * @see https://next-auth.js.org/providers/github
-         */
     ],
 }
 
@@ -63,9 +54,13 @@ export const authOptions: NextAuthOptions = {
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = (ctx: {
+export const getServerAuthSession = async (ctx: {
     req: GetServerSidePropsContext["req"]
     res: GetServerSidePropsContext["res"]
 }) => {
-    return getServerSession(ctx.req, ctx.res, authOptions)
+    const session = await getServerSession(ctx.req, ctx.res, authOptions)
+
+    if (!session) throw new Error(`User is not authenticated`)
+
+    return session.user
 }
